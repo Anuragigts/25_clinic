1733f2c89e8717ed9bbdc58d3a3a767438d713db
Anuragigts <anurag@igravitas.in>
2015-08-25 10:05:16 +0530

    clinicosys

---
+++ assets/js/custom.js
@@ -0,0 +1,377 @@
+/*=============================================================
+    Authour URI: www.binarycart.com
+    Version: 1.1
+    License: MIT
+    
+    http://opensource.org/licenses/MIT
+
+    100% To use For Personal And Commercial Use.
+   
+    ========================================================  */
+
+(function ($) {
+
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            /*====================================
+            METIS MENU 
+            ======================================*/
+            $('#main-menu').metisMenu();
+
+            /*====================================
+              LOAD APPROPRIATE MENU BAR
+           ======================================*/
+            $(window).bind("load resize", function () {
+                if ($(this).width() < 768) {
+                    $('div.sidebar-collapse').addClass('collapse')
+                } else {
+                    $('div.sidebar-collapse').removeClass('collapse')
+                }
+            });
+
+            /*====================================
+            MORRIS BAR CHART
+         ======================================*/
+            Morris.Bar({
+                element: 'morris-bar-chart',
+                data: [{
+                    y: '2006',
+                    a: 100,
+                    b: 90
+                }, {
+                    y: '2007',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2008',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2009',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2010',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2011',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2012',
+                    a: 100,
+                    b: 90
+                }],
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Series A', 'Series B'],
+                hideHover: 'auto',
+                resize: true
+            });
+
+            /*====================================
+          MORRIS DONUT CHART
+       ======================================*/
+            Morris.Donut({
+                element: 'morris-donut-chart',
+                data: [{
+                    label: "Download Sales",
+                    value: 12
+                }, {
+                    label: "In-Store Sales",
+                    value: 30
+                }, {
+                    label: "Mail-Order Sales",
+                    value: 20
+                }],
+                resize: true
+            });
+
+            /*====================================
+         MORRIS AREA CHART
+      ======================================*/
+
+            Morris.Area({
+                element: 'morris-area-chart',
+                data: [{
+                    period: '2010 Q1',
+                    iphone: 2666,
+                    ipad: null,
+                    itouch: 2647
+                }, {
+                    period: '2010 Q2',
+                    iphone: 2778,
+                    ipad: 2294,
+                    itouch: 2441
+                }, {
+                    period: '2010 Q3',
+                    iphone: 4912,
+                    ipad: 1969,
+                    itouch: 2501
+                }, {
+                    period: '2010 Q4',
+                    iphone: 3767,
+                    ipad: 3597,
+                    itouch: 5689
+                }, {
+                    period: '2011 Q1',
+                    iphone: 6810,
+                    ipad: 1914,
+                    itouch: 2293
+                }, {
+                    period: '2011 Q2',
+                    iphone: 5670,
+                    ipad: 4293,
+                    itouch: 1881
+                }, {
+                    period: '2011 Q3',
+                    iphone: 4820,
+                    ipad: 3795,
+                    itouch: 1588
+                }, {
+                    period: '2011 Q4',
+                    iphone: 15073,
+                    ipad: 5967,
+                    itouch: 5175
+                }, {
+                    period: '2012 Q1',
+                    iphone: 10687,
+                    ipad: 4460,
+                    itouch: 2028
+                }, {
+                    period: '2012 Q2',
+                    iphone: 8432,
+                    ipad: 5713,
+                    itouch: 1791
+                }],
+                xkey: 'period',
+                ykeys: ['iphone', 'ipad', 'itouch'],
+                labels: ['iPhone', 'iPad', 'iPod Touch'],
+                pointSize: 2,
+                hideHover: 'auto',
+                resize: true
+            });
+
+            /*====================================
+    MORRIS LINE CHART
+ ======================================*/
+            Morris.Line({
+                element: 'morris-line-chart',
+                data: [{
+                    y: '2006',
+                    a: 100,
+                    b: 90
+                }, {
+                    y: '2007',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2008',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2009',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2010',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2011',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2012',
+                    a: 100,
+                    b: 90
+                }],
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Series A', 'Series B'],
+                hideHover: 'auto',
+                resize: true
+            });
+           
+     
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+/************** Anurag ********
+/*  jQuery.print, version 1.0.3
+ *  (c) Sathvik Ponangi, Doers' Guild
+ * Licence: CC-By (http://creativecommons.org/licenses/by/3.0/)
+ *--------------------------------------------------------------------------*/
+
+(function($) {"use strict";
+    // A nice closure for our definitions
+
+    function getjQueryObject(string) {
+        // Make string a vaild jQuery thing
+        var jqObj = $("");
+        try {
+            jqObj = $(string).clone();
+        } catch(e) {
+            jqObj = $("<span />").html(string);
+        }
+        return jqObj;
+    }
+
+    function isNode(o) {
+        /* http://stackoverflow.com/a/384380/937891 */
+        return !!( typeof Node === "object" ? o instanceof Node : o && typeof o === "object" && typeof o.nodeType === "number" && typeof o.nodeName === "string");
+    }
+
+
+    $.print = $.fn.print = function() {
+        // Print a given set of elements
+
+        var options, $this, self = this;
+
+        // console.log("Printing", this, arguments);
+
+        if ( self instanceof $) {
+            // Get the node if it is a jQuery object
+            self = self.get(0);
+        }
+
+        if (isNode(self)) {
+            // If `this` is a HTML element, i.e. for
+            // $(selector).print()
+            $this = $(self);
+            if (arguments.length > 0) {
+                options = arguments[0];
+            }
+        } else {
+            if (arguments.length > 0) {
+                // $.print(selector,options)
+                $this = $(arguments[0]);
+                if (isNode($this[0])) {
+                    if (arguments.length > 1) {
+                        options = arguments[1];
+                    }
+                } else {
+                    // $.print(options)
+                    options = arguments[0];
+                    $this = $("html");
+                }
+            } else {
+                // $.print()
+                $this = $("html");
+            }
+        }
+
+        // Default options
+        var defaults = {
+            globalStyles : true,
+            mediaPrint : false,
+            stylesheet : null,
+            noPrintSelector : ".no-print",
+            iframe : true,
+            append : null,
+            prepend : null
+        };
+        // Merge with user-options
+        options = $.extend({}, defaults, (options || {}));
+
+        var $styles = $("");
+        if (options.globalStyles) {
+            // Apply the stlyes from the current sheet to the printed page
+            $styles = $("style, link, meta, title");
+        } else if (options.mediaPrint) {
+            // Apply the media-print stylesheet
+            $styles = $("link[media=print]");
+        }
+        if (options.stylesheet) {
+            // Add a custom stylesheet if given
+            $styles = $.merge($styles, $('<link rel="stylesheet" href="' + options.stylesheet + '">'));
+        }
+
+        // Create a copy of the element to print
+        var copy = $this.clone();
+        // Wrap it in a span to get the HTML markup string
+        copy = $("<span/>").append(copy);
+        // Remove unwanted elements
+        copy.find(options.noPrintSelector).remove();
+        // Add in the styles
+        copy.append($styles.clone());
+        // Appedned content
+        copy.append(getjQueryObject(options.append));
+        // Prepended content
+        copy.prepend(getjQueryObject(options.prepend));
+        // Get the HTML markup string
+        var content = copy.html();
+        // Destroy the copy
+        copy.remove();
+
+        var w, wdoc;
+        if (options.iframe) {
+            // Use an iframe for printing
+            try {
+                var $iframe = $(options.iframe + "");
+                var iframeCount = $iframe.length;
+                if (iframeCount === 0) {
+                    // Create a new iFrame if none is given
+                    $iframe = $('<iframe height="0" width="0" border="0" wmode="Opaque"/>').prependTo('body').css({
+                        "position" : "absolute",
+                        "top" : -999,
+                        "left" : -999
+                    });
+                }
+                w = $iframe.get(0);
+                w = w.contentWindow || w.contentDocument || w;
+                wdoc = w.document || w.contentDocument || w;
+                wdoc.open();
+                wdoc.write(content);
+                wdoc.close();
+                setTimeout(function() {
+                    // Fix for IE : Allow it to render the iframe
+                    w.focus();
+                    w.print();
+                    setTimeout(function() {
+                        // Fix for IE
+                        if (iframeCount === 0) {
+                            // Destroy the iframe if created here
+                            $iframe.remove();
+                        }
+                    }, 100);
+                }, 250);
+            } catch (e) {
+                // Use the pop-up method if iframe fails for some reason
+                console.error("Failed to print from iframe", e.stack, e.message);
+                w = window.open();
+                w.document.write(content);
+                w.document.close();
+                w.focus();
+                w.print();
+                w.close();
+            }
+        } else {
+            // Use a new window for printing
+            w = window.open();
+            w.document.write(content);
+            w.document.close();
+            w.focus();
+            w.print();
+            w.close();
+        }
+        return this;
+    };
+
+})(jQuery);
